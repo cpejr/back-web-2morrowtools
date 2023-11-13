66010a612873b9b20b1c292381e9d55d35321a2d
Samantha Kelly <samanthakelly@pop-os.localdomain>
2023-11-13 16:06:47 -0300

    feat: IA, category and routes

--- src/Controllers/IAController.js
+++ src/Controllers/IAController.js
@@ -1,28 +1,60 @@
 const { json } = require("express");
 const IAModel = require("../Models/IAModel");
 
-class IAModelController {
+class IAController {
   async create(req, res) {
-    const IA = await IAModel.create(req.body);
-
-    return res.status(200).json(IA);
+    try {
+      const IA = await IAModel.create(req.body);
+      await IA.save();  
+      res.status(200).json(IA);
+    } catch (error) {
+      res.status(500).json({ message: "ERRO", error: error.message });
+    }
   }
+
   async read(req, res) {
-    const IA = await IAModel.read(req.body);
-
-    return res.status(200).json(IA);
+    try {
+      const IA = await IAModel.read(req.body);
+      res.status(200).json(IA);
+    } catch (error) {
+      res.status(500).json({ message: "ERRO", error: error.message });
+    }
   }
-  async delet(req, res) {
-    const IA = await IAModel.delet(req.body);
 
-    return res.status(200).json(IA);
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
+
+      const IAEncontrada = await IAModel.findById(id);
+      if (!IAEncontrada) {
+        return res
+          .status(404)
+          .json({ message: "Ferramenta com id " + id + " não encontrada!" });
+      }
+      await IAEncontrada.deleteOne();
+      res
+        .status(200)
+        .json({
+          mensagem: "Ferramenta com id " + id + " deletada com sucesso!",
+        });
+    } catch (error) {
+      res.status(500).json({ message: "ERRO", error: error.message });
+    }
   }
+
   async update(req, res) {
-    const IA = await IAModel.update(req.body);
-
-    return res.status(200).json(IA);
+    try {
+      const { id } = req.params; 
+      const IAEncontrada = await IAModel.findById(id);
+      if (!IAEncontrada)
+        return res.status(404).json({ message: "Ferramenta com id " + id + " não encontrada!" });
+      const IA = await IAEncontrada.set(req.body).save(); 
+      res.status(200).json(IA); 
+    } catch (error) {
+      res.status(500).json({ message: "ERRO", error: error.message });
+    }
   }
 }
 
-module.export = new IAModelController();
+module.export = new IAController();
 module.exports = rotas;
