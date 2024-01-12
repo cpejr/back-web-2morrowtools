b773247108c8c2f3c94deccbd45b9baf3fc6826e
Rian-Rero <rian@dharma.com.br>
2024-01-11 22:29:10 -0300

    fix: removing comments

--- src/Controllers/AvaliationController.js
+++ src/Controllers/AvaliationController.js
@@ -12,7 +12,6 @@ class AvaliationController {
 
   async read(req, res) {
     try {
-      
       const category = await AvaliationModel.find(req.body);
       return res.status(200).json(category);
     } catch (error) {
@@ -53,25 +52,29 @@ class AvaliationController {
   async getByIaId(req, res) {
     try {
       const { iaId } = req.params;
-  
-      // Buscar todas as avaliações pelo iaId
       const evaluations = await AvaliationModel.find({ iaId });
-  
-      if (!evaluations || evaluations.length === 0) {
-        return res.status(404).json({ message: "No evaluations found for this IA!" });
-      }
-  
-      // Filtrar avaliações com rate válido e calcular a média
-      const validRatings = evaluations.filter(evaluation => typeof evaluation.rate === 'number' && !isNaN(evaluation.rate));
-  
-  
-      if (validRatings.length === 0) {
-        return res.status(404).json({ message: "No valid ratings found for this IA!" });
-      }
-  
-      const totalRatings = validRatings.reduce((acc, curr) => acc + curr.rate, 0);
+
+      if (!evaluations || evaluations.length === 0)
+        return res
+          .status(404)
+          .json({ message: "No evaluations found for this IA!" });
+
+      const validRatings = evaluations.filter(
+        (evaluation) =>
+          typeof evaluation.rate === "number" && !isNaN(evaluation.rate)
+      );
+
+      if (validRatings.length === 0)
+        return res
+          .status(404)
+          .json({ message: "No valid ratings found for this IA!" });
+
+      const totalRatings = validRatings.reduce(
+        (acc, curr) => acc + curr.rate,
+        0
+      );
       const averageRating = totalRatings / validRatings.length;
-  
+
       return res.status(200).json({ averageRating });
     } catch (error) {
       res.status(500).json({ message: "ERROR", error: error.message });
@@ -79,19 +82,15 @@ class AvaliationController {
   }
 
   async destroyAll(req, res) {
-  try {
-    await AvaliationModel.deleteMany({});
-    res.status(200).json({
-      message: "All avaliations successfully deleted!",
-    });
-  } catch (error) {
-    res.status(500).json({ message: "ERROR", error: error.message });
+    try {
+      await AvaliationModel.deleteMany({});
+      res.status(200).json({
+        message: "All avaliations successfully deleted!",
+      });
+    } catch (error) {
+      res.status(500).json({ message: "ERROR", error: error.message });
+    }
   }
-}
-
-
-
-  
 }
 
 module.exports = new AvaliationController();
