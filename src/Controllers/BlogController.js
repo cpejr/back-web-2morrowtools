26d5937f96ddb6c877d2f87851b47d34b58d7d1c
João Monteiro <joaomdelveaux@gmail.com>
2024-01-31 14:05:01 -0300

    fix: image no blog posts

--- src/Controllers/BlogController.js
+++ src/Controllers/BlogController.js
@@ -1,9 +1,17 @@
 const BlogModel = require("../Models/BlogModel");
+const { uploadImage, deleteImage, getImage } = require("../config/blobStorage");
+
 
 class BlogController {
   async create(req, res) {
     try {
-      const blog = await BlogModel.create(req.body);
+      const blog = await BlogModel.create({ ...req.body, imageUrl: "EMPTY" });      //save image url
+      
+      const { imageUrl: base64Image, name } = req.body;
+      const imageUrl = await uploadImage(base64Image, name);
+      blog.set({ imageUrl });
+      await blog.save();
+
       return res.status(200).json(blog);
     } catch (error) {
       res.status(500).json({ message: "Error while creating post", error: error.message });
@@ -43,6 +51,8 @@ class BlogController {
       if (!foundBlogPost) {
         return res.status(404).json({ message: "Post not found" });
       }
+
+      await deleteImage(foundBlogPost.imageUrl);
       await foundBlogPost.deleteOne();
       res.status(200).json({ message: "Post deleted sucessfully" });
     } catch (error) {
@@ -57,12 +67,27 @@ class BlogController {
       if (!foundBlogPost)
         return res.status(404).json({ message: "Post not found" });
 
-      // newimageUrl?
+        const { imageUrl } = req.body;
+        if (imageUrl) {
+          const newImageURL = await uploadImage(imageUrl, foundBlogPost.name, foundBlogPost.imageUrl);
+          await foundBlogPost.set({ ...req.body, imageUrl: newImageURL }).save();
+        }
 
       const Blog = await foundBlogPost.set(req.body).save();
       res.status(200).json(Blog);
     } catch (error) {
       res.status(500).json({ message: "ERRO", error: error.message });
+    }
+  }
+
+  async readImage(req, res) {
+    try {
+      const { imageUrl } = req.body;
+
+      const image = await getImage(imageUrl);
+      return res.status(200).json({ image });
+    } catch (error) {
+      return res.status(500).json({ message: "ERROR", error: error.message });
     }
   }
 }
--- src/Validators/BlogValidator.js
+++ src/Validators/BlogValidator.js
@@ -9,7 +9,8 @@ const create = validateRequest({
       .min(2, { message: "Name must be at least 2 characters long" }),
     imageUrl: z.string({ required_error: "The Url is required" }),
     shortDescription: z.string({ required_error: "The description is required" }),
-    longDescription: z.string({ required_error: "The description is required" }),
+    longDescription: z.string({ required_error: "The description is required" })
+    .min(20, { message: "Long description must be at least 20 characters long" }),
     id_categoryfeature: z.string({ required_error: "Invalid ID for Category feature" }),
     id_categoryprofession: z.string({ required_error: "Invalid ID for Category profession" }),
   }),
@@ -26,10 +27,10 @@ const update = validateRequest({
     id: z.custom(mongoose.isValidObjectId, "The Id is not valid"),
   }),
   body: z.object({
-    name: z.string().optional,
-    imageUrl: z.string().optional,
-    shortDescription: z.string().optional,
-    longDescription: z.string().optional,
+    name: z.string().optional(),
+    imageUrl: z.string().optional(),
+    shortDescription: z.string().optional(),
+    longDescription: z.string().optional(),
     id_categoryfeature: z
       .custom(mongoose.isValidObjectId, "The category feature ID is not valid")
       .optional(),
