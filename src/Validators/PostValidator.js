427755fd63045646af2c300485bc72bf426e2abf
SamuelFVG <samuelfelipeif@gmail.com>
2024-02-29 13:11:54 -0300

    feat: increase blog text size

--- src/Validators/PostValidator.js
+++ src/Validators/PostValidator.js
@@ -7,22 +7,22 @@ const create = validateRequest({
     name: z
       .string({ required_error: "The post require a name" })
       .min(2, { message: "Name must be at least 2 characters long" }),
-    imageUrl: 
-      z.string({ required_error: "A URL da imagem é obrigatória" })
+    imageUrl: z
+      .string({ required_error: "A URL da imagem é obrigatória" })
       .min(1, { message: "O URL deve ter pelo menos 1 caractere" }),
     shortDescription: z
       .string({ required_error: "The short description is required" })
       .min(2, {
         message: "Short description must be at least 2 characters long",
       })
-      .max(100, { message: "Short description must not exceed 60 characters" }),
+      .max(1000, { message: "Short description must not exceed 1000 characters" }),
 
     longDescription: z
       .string({ required_error: "The large description is required" })
       .min(20, {
         message: "Large description must be at least 20 characters long",
       })
-      .max(750, { message: "Large description must not exceed 500 characters" }),
+      .max(7500, { message: "Large description must not exceed 7500 characters" }),
     id_categoryfeature: z
       .array(z.custom(mongoose.isValidObjectId, "The category feature ID is not valid"))
       .min(1, "At least one category feature ID is required"),
@@ -47,7 +47,7 @@ const update = validateRequest({
     imageUrl: z.string().optional(),
     shortDescription: z.string().optional(),
     longDescription: z.string().optional(),
-    id_categoryfeatures: z  
+    id_categoryfeatures: z
       .array(z.custom(mongoose.isValidObjectId, "The category feature ID is not valid"))
       .optional(),
     id_categoryprofessions: z
